8409f6b90a87c045842958f9ab117108034a97ca
Roshan Shrestha <roshan.shrestha4u@gmail.com>
2025-03-04 18:17:00 +0545

    display the name and size of file/dir

--- main.go
+++ main.go
@@ -6,14 +6,21 @@ import (
 	"os"
 )
 
+const KB = 1024
+
 func main() {
+
 	dir, err := os.ReadDir("./")
 	if err != nil {
 		log.Fatal("Something went wrong", err)
 	}
 
 	for _, value := range dir {
-		fmt.Println(value.Name())
+		file, err := value.Info()
+		if err != nil {
+			log.Fatal("something went wrong", err)
+		}
+		fmt.Printf(">> %s - %.2f KB\n", file.Name(), float64(file.Size())/KB)
 	}
 
 }
