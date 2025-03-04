0126e5cb1542fc78e80b12df80681fe63d6d26d9
Roshan Shrestha <roshan.shrestha4u@gmail.com>
2025-03-04 17:42:21 +0545

    basic listing of the files

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	dir, err := os.ReadDir("./")
+	if err != nil {
+		log.Fatal("Something went wrong", err)
+	}
+
+	for _, value := range dir {
+		fmt.Println(value)
+	}
+
 }
