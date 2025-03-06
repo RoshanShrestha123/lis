07a68036868670f755b2512d2d9950c323a4cc6f
Roshan Shrestha <roshan.shrestha4u@gmail.com>
2025-03-06 18:23:58 +0545

    color define for dir and files

--- main.go
+++ main.go
@@ -11,14 +11,17 @@ const KB = 1024
 
 func main() {
 
+	var Reset = "\033[0m"
+	var Red = "\033[31m"
+	var Green = "\033[32m"
+
 	dir, err := os.ReadDir("./")
 	if err != nil {
 		log.Fatal("Something went wrong", err)
 	}
 
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', tabwriter.Debug)
-	fmt.Fprintln(w, "Names\tSize(KB)\t")
-	fmt.Fprintln(w, "-\t-\t")
+	fmt.Fprintln(w, Green+"Names\tSize(KB)"+Reset+"\t")
 
 	for _, value := range dir {
 		file, err := value.Info()
@@ -26,7 +29,14 @@ func main() {
 			log.Fatal("something went wrong", err)
 		}
 
-		fmt.Fprintf(w, "%s\t%f\t\n", file.Name(), float32(file.Size())/KB)
+		var selectedColor string
+		if file.IsDir() {
+			selectedColor = Red
+		} else {
+			selectedColor = Green
+		}
+
+		fmt.Fprintf(w, selectedColor+"%s\t%.2f"+Reset+"\t\n", file.Name(), float32(file.Size())/KB)
 
 	}
 
