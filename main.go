76bcabc9a8215cd34fff83a58c951ae24eca5f6f
Roshan Shrestha <roshan.shrestha4u@gmail.com>
2025-03-06 18:11:53 +0545

    proper formating while display

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"text/tabwriter"
 )
 
 const KB = 1024
@@ -15,12 +16,20 @@ func main() {
 		log.Fatal("Something went wrong", err)
 	}
 
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', tabwriter.Debug)
+	fmt.Fprintln(w, "Names\tSize(KB)\t")
+	fmt.Fprintln(w, "-\t-\t")
+
 	for _, value := range dir {
 		file, err := value.Info()
 		if err != nil {
 			log.Fatal("something went wrong", err)
 		}
-		fmt.Printf(">> %s - %.2f KB\n", file.Name(), float64(file.Size())/KB)
+
+		fmt.Fprintf(w, "%s\t%f\t\n", file.Name(), float32(file.Size())/KB)
+
 	}
+
+	w.Flush()
 
 }
